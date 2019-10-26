46edd6dfb8af89792f5e48b84b2559d0c9430bb0
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-10-26 13:49:35 -0400

    Add files via upload

--- gradient_descent_logistic_regression.py
+++ gradient_descent_logistic_regression.py
@@ -18,78 +18,58 @@ class GradientDescentLogisticRegression:
     def __init__(self,X,y,learning_rate,max_iter,print_theta=False):
         self.X = X 
         self.X1 = np.matrix(sm.add_constant(X)) #Adds a column of ones to the front of X  
-        self.y = y 
-        self.m_y = np.shape(self.y)
-        self.y = np.reshape(self.y, (self.m_y[0],1))
-        self.m,self.n = np.shape(self.X1)
+        self.m,self.n = self.X1.shape
+        self.y = y.as_matrix().reshape((self.m,1))
         self.theta = np.ones((self.n, 1)) #initialize "random" theta as a column of ones
         self.learning_rate = learning_rate
         self.max_iter = max_iter
         self.print_theta = print_theta 
         
-    def calculate_zed(self): 
-        '''
-        Calculate zed.  
-        '''
-        theta = self.theta
-        zed = np.matmul(self.X1,theta) 
-        return(zed)  
+    def pred(self, theta, x): 
+        prediction = np.matmul(np.transpose(theta), np.transpose(x))
+        return 1/(1+np.exp(-prediction))
         
-    def calculate_predictions(self): 
-        '''
-        Calculates the predictions using the LogReg hypothesis formula.  
-        '''
-        zed = self.calculate_zed() 
-        denominator_second_term = np.exp(-zed)
-        denominator = 1 + denominator_second_term 
-        predictions = 1/denominator
-        #print(predictions)
-        return(predictions) 
-        
-    def cross_entropy_objective(self): 
+    def loss_function(self,theta): 
         '''
         Calculate the Cross Entropy Objective, our Loss Func. for Log. Reg.
         '''
-        y = self.y
-        m_y = np.shape(y)
-        ones = np.ones(m_y)
-        predictions = self.calculate_predictions()
-        log_predictions = np.log(predictions)
-        ones_minus_y = np.subtract(ones, y)
-        loss = np.dot(np.transpose(y), log_predictions) + np.dot(np.transpose(ones_minus_y),log_predictions)
-        loss = -loss
-        return(loss)
+#         zed = np.matmul(self.X1,theta)
+#         print(zed)
+#         predictions = 1/(1 + np.exp(-zed))
+#         ceo = -1 * (np.matmul(np.transpose(self.y),np.log(predictions)) + np.matmul(np.transpose(1-self.y),np.log(np.absolute(1-predictions))))
+        total = 0
+        for i in range(self.m): 
+            pred = self.pred(theta, self.X1[i,:])
+            total += self.y[i]*np.log(pred) + (1-self.y[i]*np.log(1-pred))
+        return -total
+#         #print(np.log(np.absolute(1-h))) #can't take the log of a negative number.  An appropriate solution?  
+#         return(ceo)
         
-    def calculate_f1_score(self): 
+    def calculate_f1_score(self,theta): 
         '''
         Calculates the F1 Score using metrics from SKLearn.  
         '''
-        y = self.y
-        predictions = self.calculate_predictions()  
-        f1_score = metrics.f1_score(y, predictions.round(), labels = None, pos_label=1, average='binary', sample_weight=None) 
-        return(f1_score)
+        zed = np.matmul(self.X1,theta)
+        predictions = 1/(1 + np.exp(-zed))
+        f1_score = metrics.f1_score(self.y, np.around(predictions), labels = None, pos_label=1, average='binary', sample_weight=None)
+        return(f1_score) #F1 Score doesn't seem to vary when changing iterations or learning rate.  Why?  
     
     def gradient_descent(self): 
         '''
         Calculates gradient descent for Logistic Regression. 
         '''
         print_theta = self.print_theta
-        X1 = self.X1
-        y = self.y
-        learning_rate = self.learning_rate 
+        alpha = self.learning_rate #alpha represents learning_rate
         max_iter = self.max_iter
         theta = self.theta 
         for i in range(0, max_iter):
-            loss = self.cross_entropy_objective()
-            predictions = self.calculate_predictions()
-            innermost_term = (predictions-y)
-            inner_term = np.matmul(np.transpose(X1),innermost_term) 
-            step = learning_rate*inner_term 
+            loss = self.loss_function(theta)
+            zed = np.matmul(self.X1,theta)
+            predictions = 1/(1+ (1/np.exp(zed)))
+            step = alpha*(np.matmul(np.transpose(self.X1),(np.around(predictions)-self.y)))
             theta = theta-step 
-        f1_score = self.calculate_f1_score()
+        f1_score = self.calculate_f1_score(theta)
         if print_theta==True:
             print("Theta: ", theta)
-        print("\nCross Entropy: %f" % (loss), "\nAlpha = %s" % learning_rate, "\nIterations: %s" % max_iter, "\nF1 Score: ", f1_score)
-        #if max_iter==100: 
-        #    print("F1 Score: ", f1_score)
+        print("\nCross Entropy: %f" % (loss), "\nAlpha = %s" % alpha, "\nIterations: %s" % max_iter, "\nF1 Score: ", f1_score)
         return(None)
